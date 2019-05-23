e59ab8bdb6b3fe421442d78de868cdb0595f8f23
M1F1 <filek.michal@gmail.com>
2019-05-23 15:39:40 +0200

    add saving .pb

--- data_preparation.py
+++ data_preparation.py
@@ -1,26 +1,7 @@
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
-import nltk
-from nltk.corpus import stopwords
 import re
-# stopwords = set(stopwords.words('english'))
-# print(stopwords)
-# with open('stopwords.txt', 'w') as f:
-#     for item in stopwords:
-#         f.write("%s\n" % item)
-
-df = pd.read_csv(os.path.join(os.getcwd(), 'data', 'train.csv'))
-df['toxic'] = df.iloc[:, 2:].any(axis='columns').astype(int)
-del df['id']
-df = df.iloc[:, 0:2]
-
-# get balanced distribution
-df_positive = df.loc[df['toxic'] == 1]
-positive_count = df_positive.shape[0]
-df_negative = df.loc[df['toxic'] == 0][:positive_count]
-df = pd.concat([df_positive, df_negative])
-print(df.head())
 
 
 def clean_string(s,
@@ -41,18 +22,32 @@ def clean_string(s,
             stopwords = [line.rstrip('\n') for line in f]
         s = ' '.join([word for word in s.split() if word not in stopwords])
     if remove_digits:
-        pass
-    # TODO: implement remove digits
+        s = re.sub(r"[0-9]+", '', s)
     return s
 
 
-stopwords_path = os.path.join(os.getcwd(), 'stopwords.txt')
-# data_preprocessing
-df['comment_text'] = df['comment_text'].apply(lambda x: clean_string(x, stopwords_path))
+if __name__ == '__main__':
+    df = pd.read_csv(os.path.join(os.getcwd(), 'data', 'train.csv'))
+    df['toxic'] = df.iloc[:, 2:].any(axis='columns').astype(int)
+    del df['id']
+    df = df.iloc[:, 0:2]
 
-df.to_csv(os.path.join(os.getcwd(), 'data', 'new_labels_train.csv'))
-print(df.head()['comment_text'])
+    # get balanced distribution
+    df_positive = df.loc[df['toxic'] == 1]
+    positive_count = df_positive.shape[0]
+    df_negative = df.loc[df['toxic'] == 0][:positive_count]
+    df = pd.concat([df_positive, df_negative])
+    print(df.head())
 
-df.hist(column='toxic')
-plt.show()
+    # data_preprocessing
+    stopwords_path = os.path.join(os.getcwd(), 'stopwords.txt')
+    df['comment_text'] = df['comment_text'].apply(lambda x: clean_string(x, stopwords_path))
+
+    # save processed dataframe
+    df.to_csv(os.path.join(os.getcwd(), 'data', 'processed_data.csv'))
+    print(df.head()['comment_text'])
+
+    # show classes histogram
+    df.hist(column='toxic')
+    plt.show()
 
--- train.py
+++ train.py
@@ -1,55 +1,57 @@
 import os
-import sys
 import numpy as np
 import pandas as pd
-import keras
-from keras.preprocessing.text import Tokenizer
-from keras.utils import to_categorical
-from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Activation, Conv2D, Input, Embedding, Reshape, MaxPool2D, Concatenate, Flatten, Dropout, Dense, Conv1D
-from keras.models import Model
-from keras.layers import Embedding
-from keras.callbacks import ModelCheckpoint
-from keras.optimizers import Adam
+import tensorflow as tf
+from tensorflow import keras
+import time
 import json
+import sys
+
+
+assert sys.version_info >= (3, 5) # Python ≥3.5 required
+assert tf.__version__ >= "2.0"    # TensorFlow ≥2.0 required
 
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-df = pd.read_csv('data/new_labels_train.csv')
+df = pd.read_csv(config['TEXT_DATA_PATH'])
 texts = (np.array(df['comment_text'].astype(str)))
 labels = list(df['toxic'])
-print(type(texts[0]))
-tokenizer = Tokenizer(num_words=config['MAX_WORDS'])
+
+tokenizer = keras.preprocessing.text.Tokenizer(num_words=config['MAX_WORDS'])
 tokenizer.fit_on_texts(texts)
 sequences = tokenizer.texts_to_sequences(texts)
 word_index = tokenizer.word_index
 print("unique words : {}".format(len(word_index)))
 
-data = pad_sequences(sequences, maxlen=config['MAX_SEQUENCE_LENGTH'])
-
-labels = to_categorical(np.asarray(labels))
+data = keras.preprocessing.sequence.pad_sequences(sequences, maxlen=config['MAX_SEQUENCE_LENGTH'])
+labels = keras.utils.to_categorical(np.asarray(labels))
 print('Shape of data tensor:', data.shape)
 print('Shape of label tensor:', labels.shape)
-print(labels)
 
 # split the data into a training set and a validation set
 indices = np.arange(data.shape[0])
 np.random.shuffle(indices)
 data = data[indices]
 labels = labels[indices]
+# data = data[:1000]
+# labels = labels[:1000]
 nb_validation_samples = int(config['VALIDATION_SPLIT'] * data.shape[0])
-
-x_train = data[:-nb_validation_samples]
-y_train = labels[:-nb_validation_samples]
-x_val = data[-nb_validation_samples:]
-y_val = labels[-nb_validation_samples:]
+x_train = data[:nb_validation_samples]
+y_train = labels[:nb_validation_samples]
+x_val = data[nb_validation_samples:-config['TEST_SAMPLES_NUM']]
+y_val = labels[nb_validation_samples:-config['TEST_SAMPLES_NUM']]
+x_test = data[-config['TEST_SAMPLES_NUM']:]
+y_test = labels[-config['TEST_SAMPLES_NUM']:]
 
 print('Shape of data train tensor:', x_train.shape)
 print('Shape of label train tensor:', y_train.shape)
-print('Shape of data train tensor:', x_val.shape)
-print('Shape of label train tensor:', y_val.shape)
+print('Shape of data val tensor:', x_val.shape)
+print('Shape of label val tensor:', y_val.shape)
+print('Shape of data test tensor:', x_test.shape)
+print('Shape of label test tensor:', y_test.shape)
 
+# create embeddings
 embeddings_index = {}
 with open(os.path.join(config['GLOVE_DIR'])) as f:
     for line in f:
@@ -65,22 +67,23 @@ for word, i in word_index.items():
     embedding_vector = embeddings_index.get(word)
     if embedding_vector is not None:
         in_glove += 1
-        # words not found in embedding index will be all-zeros.
         embedding_matrix[i] = embedding_vector
-print(in_glove)
+
+print('Number of words in dictionary that have embeddings in glove:', in_glove)
 
 
-embedding_layer = Embedding(len(word_index) + 1,
-                            config['EMBEDDING_DIM'],
-                            weights=[embedding_matrix],
-                            input_length=config['MAX_SEQUENCE_LENGTH'],
-                            trainable=False)
+# setting up model architecture
+embedding_layer = keras.layers.Embedding(len(word_index) + 1,
+                                         config['EMBEDDING_DIM'],
+                                         weights=[embedding_matrix],
+                                         input_length=config['MAX_SEQUENCE_LENGTH'],
+                                         trainable=False)
 
-inputs = Input(shape=(config['MAX_SEQUENCE_LENGTH'],), dtype='int32')
+inputs = keras.layers.Input(shape=(config['MAX_SEQUENCE_LENGTH'],), dtype='int32')
 embedding = embedding_layer(inputs)
 print(embedding.shape)
 
-reshape = Reshape((config['MAX_SEQUENCE_LENGTH'], config['EMBEDDING_DIM'], 1))(embedding)
+reshape = keras.layers.Reshape((config['MAX_SEQUENCE_LENGTH'], config['EMBEDDING_DIM'], 1))(embedding)
 print(reshape.shape)
 
 num_filters = config['NUM_FILTERS']
@@ -90,52 +93,47 @@ filter_size_1 = config['FILTER_SIZE_1']
 filter_size_2 = config['FILTER_SIZE_2']
 filter_size_3 = config['FILTER_SIZE_3']
 
-conv_0 = Conv2D(num_filters,
-                kernel_size=(filter_size_1, embedding_dim),
-                padding='valid',
-                kernel_initializer='normal',
-                activation='relu')(reshape)
-conv_1 = Conv2D(num_filters,
-                kernel_size=(filter_size_2, embedding_dim),
-                padding='valid',
-                kernel_initializer='normal',
-                activation='relu')(reshape)
-conv_2 = Conv2D(num_filters,
-                kernel_size=(filter_size_3, embedding_dim),
-                padding='valid',
-                kernel_initializer='normal',
-                activation='relu')(reshape)
+conv_0 = keras.layers.Conv2D(num_filters,
+                             kernel_size=(filter_size_1, embedding_dim),
+                             padding='valid',
+                             kernel_initializer='normal',
+                             activation='relu')(reshape)
+conv_1 = keras.layers.Conv2D(num_filters,
+                             kernel_size=(filter_size_2, embedding_dim),
+                             padding='valid',
+                             kernel_initializer='normal',
+                             activation='relu')(reshape)
+conv_2 = keras.layers.Conv2D(num_filters,
+                             kernel_size=(filter_size_3, embedding_dim),
+                             padding='valid',
+                             kernel_initializer='normal',
+                             activation='relu')(reshape)
 
-maxpool_0 = MaxPool2D(pool_size=(max_sequence_length - filter_size_1 + 1, 1),
-                      strides=(1,1),
-                      padding='valid')(conv_0)
-maxpool_1 = MaxPool2D(pool_size=(max_sequence_length - filter_size_2 + 1, 1),
-                      strides=(1,1),
-                      padding='valid')(conv_1)
-maxpool_2 = MaxPool2D(pool_size=(max_sequence_length - filter_size_3 + 1, 1),
-                      strides=(1,1),
-                      padding='valid')(conv_2)
+maxpool_0 = keras.layers.MaxPool2D(pool_size=(max_sequence_length - filter_size_1 + 1, 1),
+                                   strides=(1, 1),
+                                   padding='valid')(conv_0)
+maxpool_1 = keras.layers.MaxPool2D(pool_size=(max_sequence_length - filter_size_2 + 1, 1),
+                                   strides=(1, 1),
+                                   padding='valid')(conv_1)
+maxpool_2 = keras.layers.MaxPool2D(pool_size=(max_sequence_length - filter_size_3 + 1, 1),
+                                   strides=(1, 1),
+                                   padding='valid')(conv_2)
 
-concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
-flatten = Flatten()(concatenated_tensor)
-dropout = Dropout(config['DROP'])(flatten)
-output = Dense(units=2,
-               activation='softmax')(dropout)
+concatenated_tensor = keras.layers.Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
+flatten = keras.layers.Flatten()(concatenated_tensor)
+dropout = keras.layers.Dropout(config['DROP'])(flatten)
+output = keras.layers.Dense(units=2,
+                            activation='softmax')(dropout)
 
 # this creates a model that includes
-model = Model(inputs=inputs,
-              outputs=output)
+model = keras.models.Model(inputs=inputs,
+                           outputs=output)
 
-checkpoint = ModelCheckpoint('weights_cnn_sentece.hdf5',
-                             monitor='val_acc',
-                             verbose=1,
-                             save_best_only=True,
-                             mode='auto')
-adam = Adam(lr=1e-4,
-            beta_1=0.9,
-            beta_2=0.999,
-            epsilon=1e-08,
-            decay=0.0)
+adam = keras.optimizers.Adam(lr=1e-4,
+                             beta_1=0.9,
+                             beta_2=0.999,
+                             epsilon=1e-08,
+                             decay=0.0)
 
 model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
 model.summary()
@@ -146,8 +144,20 @@ model.fit(x_train,
           batch_size=config['BATCH_SIZE'],
           epochs=config['EPOCHS'],
           verbose=1,
-          callbacks=[checkpoint],
-          validation_data=(x_val, y_val))
+          validation_data=(x_val, y_val),
+          )
+print("Evaluating Model...")
+model.evaluate(x_test, y_test)
 
-
+# Saving model to .h5 and converting to .pb for tf serving
+all_models_path = 'models'
+model_name = "cnn"
+model_version = int(time.time())
+model_path = os.path.join(all_models_path, model_name, str(model_version))
+os.makedirs(model_path)
+model_saved_path = os.path.join(model_path, 'model.h5')
+model.save(model_saved_path)
+tf.keras.backend.set_learning_phase(0) # Ignore dropout at inference
+model = tf.keras.models.load_model(model_saved_path)
+tf.keras.experimental.export_saved_model(model, os.path.join(model_path, 'tf_serving_model'))
 
